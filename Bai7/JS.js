f72da54468dab3b71c7a062a1ed7032dcac1f51e
thine <thinhcubi27@gmail.com>
2021-04-12 13:34:49 +0700

    Bai7

---
+++ Bai7/JS.js
@@ -0,0 +1,11 @@
+let a=+prompt("Nhap a")
+let b=+prompt("Nhap b")
+let c=+prompt("Nhap c")
+D=b**b-(4*a*c)
+if (D<0) {
+    alert("PT vo nghiem")
+} else if (D=0) {
+    alert("PT co nghiem kep x1=x2")
+} else {
+    alert("PT co 2 nghiem phan biet")
+}
