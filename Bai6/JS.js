160200033b9188317dc11ce796ce79bc70bf72b4
thine <thinhcubi27@gmail.com>
2021-04-12 13:34:36 +0700

    Bai6

---
+++ Bai6/JS.js
@@ -0,0 +1,10 @@
+let a=+prompt("Nhap a")
+let b=+prompt("Nhap b")
+x=-b/a
+if (a!=0) {
+    alert("PT co nghiem x=" + x)
+} else if (b!=0) {
+    alert("PT vo nghiem")
+} else {
+    alert ("PT vo so nghiem")
+}
